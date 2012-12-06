844d10a45bd1d5bf3d2e8c05e83a37802acf6e14
Christopher Tang <ctang4@umbc.edu>
2012-12-06 16:22:21 -0500

    Update 12/6/12 4:00PM

--- proj2.rb
+++ proj2.rb
@@ -24,7 +24,7 @@ class Proj2 < App
     #  |dc|
     #  dc.draw_bitmap(logo, 10, 10, false) }
     
-    #Prefered method of displaying images, imo
+    #Preferred method of displaying images, imo
     logoButton = BitmapButton.new(@mainFrame, :bitmap => logo, :style => SUNKEN_BORDER) 
     logoSizer.add(logoButton, 0, ALIGN_CENTER, 2)
      
@@ -66,20 +66,71 @@ class Proj2 < App
 
   def instructions
     
+    keySizer = GridSizer.new(2)
     #Display game instructions. Make it graphical.
+    wButton = StaticBitmap.new(@mainFrame, :label => Bitmap.new("images\\wkey.bmp")) 
+    keySizer.add(wButton, 0, ALIGN_CENTER, 2)
+    
+    wLabel = StaticText.new(@mainFrame, :label => "Moves forward\n")
+    wLabel.set_foreground_colour(Wx::RED)
+    keySizer.add(wLabel, 0, ALIGN_LEFT, 2)
+    
+    sButton = StaticBitmap.new(@mainFrame, :label => Bitmap.new("images\\skey.bmp")) 
+    keySizer.add(sButton, 0, ALIGN_CENTER, 2)
+    
+    sLabel = StaticText.new(@mainFrame, :label => "Moves backward")
+    sLabel.set_foreground_colour(Wx::RED)
+    keySizer.add(sLabel, 0, ALIGN_LEFT, 2)
+        
+    aButton = StaticBitmap.new(@mainFrame, :label => Bitmap.new("images\\akey.bmp")) 
+    keySizer.add(aButton, 0, ALIGN_CENTER, 2)
+    
+    aLabel = StaticText.new(@mainFrame, :label => "Moves left")
+    aLabel.set_foreground_colour(Wx::RED)
+    keySizer.add(aLabel, 0, ALIGN_LEFT, 2)
+    
+    dButton = StaticBitmap.new(@mainFrame, :label => Bitmap.new("images\\dkey.bmp")) 
+    keySizer.add(dButton, 0, ALIGN_CENTER, 2)
+    
+    dLabel = StaticText.new(@mainFrame, :label => "Moves right")
+    dLabel.set_foreground_colour(Wx::RED)
+    keySizer.add(dLabel, 0, ALIGN_LEFT, 2)
+    
+    qButton = StaticBitmap.new(@mainFrame, :label => Bitmap.new("images\\qkey.bmp")) 
+    keySizer.add(qButton, 0, ALIGN_CENTER, 2)
+    
+    qLabel = StaticText.new(@mainFrame, :label => "Quits the game")
+    qLabel.set_foreground_colour(Wx::RED)
+    keySizer.add(qLabel, 0, ALIGN_LEFT, 2)
+    
+    eButton = StaticBitmap.new(@mainFrame, :label => Bitmap.new("images\\ekey.bmp")) 
+    keySizer.add(eButton, 0, ALIGN_CENTER, 2)
+    
+    eLabel = StaticText.new(@mainFrame, :label => "Grabs the block to move it")
+    eLabel.set_foreground_colour(Wx::RED)
+    keySizer.add(eLabel, 0, ALIGN_LEFT, 2)
+    
+    rButton = StaticBitmap.new(@mainFrame, :label => Bitmap.new("images\\rkey.bmp")) 
+    keySizer.add(rButton, 0, ALIGN_CENTER, 2)
+    
+    rLabel = StaticText.new(@mainFrame, :label => "Resets the level")
+    rLabel.set_foreground_colour(Wx::RED)
+    keySizer.add(rLabel, 0, ALIGN_LEFT, 2)
+        
+    #ADD PLAYER, BLOCK, AND GOAL TO INSTRUCTIONS!
     
     backButton = Button.new(@mainFrame, -1, "Back")
     
+    @menuSizer.add(keySizer,0,ALIGN_CENTER,2)
     @menuSizer.add(backButton, 0, ALIGN_CENTER, 2)
-        
+    
     @mainFrame.evt_button(backButton.get_id) { 
       @menuSizer.show(false)
       @menuSizer.clear()
       @menuSizer.layout()
       displayMenu }
       
-    @menuSizer.layout()
-    
+    @menuSizer.layout()    
   end
   
   def levelSelect
@@ -100,7 +151,7 @@ class Proj2 < App
     #Launch game window
     @mainFrame.evt_button(selectButton.get_id) {
       level = levelListBox.get_selections()
-    playGame(levelListBox.get_string(level[0])) }
+    loadGame(levelListBox.get_string(level[0])) }
     @mainFrame.evt_button(backButton.get_id) { 
       @menuSizer.show(false)
       @menuSizer.clear()
@@ -111,13 +162,18 @@ class Proj2 < App
 
   end
   
-  def playGame(filename)
+  def loadGame(filename)
     file = File.open("levels\\" + filename, "r")
     puts(filename)
     
+    gameSize = Size.new(500, 500)
+    @gameFrame = Frame.new(nil, :title => filename, :size => gameSize)
+    
+    @gameFrame.show()
+    
     #Testing to read levels
     puts(File.read(file))
-    data = File.read(file).split("")
+    data = File.read(file).split(",")
     convertedData = ""
     
     for i in data
@@ -131,6 +187,8 @@ class Proj2 < App
         convertedData.concat("B")
       elsif i == "3"
         convertedData.concat("G")
+      elsif i == "\n"
+        convertedData.concat("\n")
       else
         convertedData.concat(i)
       end
