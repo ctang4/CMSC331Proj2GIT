f5afd37421f9ec4b20026eae96aed7a74af9d93a
Christopher Tang <ctang4@umbc.edu>
2012-12-11 11:02:24 -0500

    Nearly Done

--- proj2.rb
+++ proj2.rb
@@ -35,14 +35,22 @@ class Proj2 < App
     
     #Make button graphics
     
-    playButton = BitmapButton.new(@mainFrame, -1, :bitmap => Bitmap.new("images\\buttonPlay.bmp"), :style => SUNKEN_BORDER)
-    infoButton = BitmapButton.new(@mainFrame, -1, :bitmap => Bitmap.new("images\\backupButton.bmp"), :style => SUNKEN_BORDER)
-    quitButton = Button.new(@mainFrame, -1, "Quit")
+    playButton = BitmapButton.new(@mainFrame, -1, 
+      :bitmap => Bitmap.new("images\\buttonPlay.bmp"), :style => SUNKEN_BORDER)
+    infoButton = BitmapButton.new(@mainFrame, -1, 
+      :bitmap => Bitmap.new("images\\backupButton.bmp"), :style => SUNKEN_BORDER)
+    quitButton = BitmapButton.new(@mainFrame, -1, 
+      :bitmap => Bitmap.new("images\\buttonQuit.bmp"), :style => SUNKEN_BORDER)
     
     @menuSizer.insert_spacer(-1, 20)
     @menuSizer.add(playButton, 0, ALIGN_CENTER, 2)
-    @menuSizer.add(infoButton, 0, ALIGN_CENTER, 2) 
-    @menuSizer.add(quitButton, 0, ALIGN_CENTER, 2)
+    
+    tempSizer = BoxSizer.new(VERTICAL)
+    tempSizer.add(infoButton, 0, ALIGN_CENTER, 2) 
+    tempSizer.add(quitButton, 0, ALIGN_CENTER, 2)
+    tempSizer.insert_spacer(-1, 20)
+    
+    @menuSizer.add(tempSizer, 0, ALIGN_CENTER, 2)
     
     @mainFrame.evt_button(playButton.get_id) { 
       @menuSizer.show(false)
@@ -166,36 +174,47 @@ class Proj2 < App
     #Testing to read levels
     data = File.read(file).split
     convertedData = ""
-    numCols = Integer(data[0])
-    numRows = Integer(data[1])
+    @numCols = Integer(data[0])
+    @numRows = Integer(data[1])
 
     #Remove Row and Column data
     data.delete_at(0)
     data.delete_at(0) 
     
-    gameSize = Size.new(53 * numCols, 49 * numRows)
+    gameSize = Size.new(53 * @numCols, 49 * @numRows)
     @gameFrame = Frame.new(nil, 2, :title => filename, :size => gameSize)
    
-    @mapGrid = GridSizer.new(numRows, numCols, 0, 0)
+    @mapGrid = GridSizer.new(@numRows, @numCols, 0, 0)
     
-    indexToAdd = 1
-    
+    indexToAdd = 0
+    @spaceIndex = Array.new
+      
     for i in data
       if i == "x"
-        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, :label => Bitmap.new("images\\wallBrown.bmp")), 0, EXPAND)
+        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, 
+          :label => Bitmap.new("images\\wallBrown.bmp")), 0, EXPAND)
         indexToAdd += 1
       elsif i == "o"
-        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, :label => Bitmap.new("images\\basicSquare.bmp")), 0, EXPAND)
+        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, 
+          :label => Bitmap.new("images\\basicSquare.bmp")), 0, EXPAND)
+        @spaceIndex.push(indexToAdd)
         indexToAdd += 1
       elsif i == "1"
-        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, :label => Bitmap.new("images\\walkDown.bmp")), 0, EXPAND)
+        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, 
+          :label => Bitmap.new("images\\walkDown.bmp")), 0, EXPAND)
+        @playerIndex = indexToAdd
         indexToAdd += 1
       elsif i == "2"
-        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, :label => Bitmap.new("images\\wallGreen.bmp")), 0, EXPAND)
-        indexToAdd += 1
+        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, 
+          :label => Bitmap.new("images\\wallGreen.bmp")), 0, EXPAND)
+        @blockIndex = indexToAdd
+        indexToAdd += 1      
       elsif i == "3"
-        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, :label => Bitmap.new("images\\goalSquare.bmp")), 0, EXPAND)  
-        indexToAdd += 1
+        @mapGrid.add(StaticBitmap.new(@gameFrame, indexToAdd, 
+          :label => Bitmap.new("images\\goalSquare.bmp")), 0,  EXPAND)  
+        @goalIndex = indexToAdd
+        @spaceIndex.push(indexToAdd)
+        indexToAdd += 1      
       end
     end
         
@@ -209,12 +228,285 @@ class Proj2 < App
     
     @gameFrame.show()
     
-    playGame
+    playGame(filename)
     
   end
   
-  def playGame
+  def playGame(filename)
     
+    playerFacing = "s"
+    playerGrabbing = false
+
+    @gameFrame.evt_key_down { |ev| 
+      if ev.get_key_code() == 69 #E - Grab
+        
+        if playerGrabbing == true
+          playerGrabbing = false   
+        elsif playerFacing == "n"
+          if @playerIndex - @numCols == @blockIndex
+            playerGrabbing = true
+          end
+        elsif playerFacing == "e"
+          if @playerIndex + 1 == @blockIndex
+            playerGrabbing = true
+          end
+        elsif playerFacing == "s"
+          if @playerIndex + @numCols == @blockIndex
+            playerGrabbing = true
+          end
+        elsif playerFacing == "w"
+          if @playerIndex - 1 == @blockIndex
+            playerGrabbing = true
+          end
+        end
+        
+      elsif ev.get_key_code() == 65 #A - West
+        
+        if playerGrabbing == false
+          spaceAhead = canMove?("w", 0)
+          playerFacing = "w"
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex - 1).get_window().
+              set_bitmap(Bitmap.new("images\\walkLeft.bmp"))
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@playerIndex - 1)
+            @spaceIndex.push(@playerIndex)
+            @playerIndex = @playerIndex - 1
+          else
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\walkLeft.bmp"))
+          end
+        elsif playerGrabbing == true && playerFacing == "w"
+          spaceAhead = canMove?("w", 1)
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex - 1).get_window().
+              set_bitmap(Bitmap.new("images\\walkLeft.bmp"))
+            @mapGrid.get_item(@blockIndex - 1).get_window().
+              set_bitmap(Bitmap.new("images\\wallGreen.bmp"))
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@blockIndex - 1)
+            @spaceIndex.push(@playerIndex)
+            @playerIndex = @playerIndex - 1
+            @blockIndex = @blockIndex - 1
+           end
+        elsif playerGrabbing == true && playerFacing == "e"
+          spaceAhead = canMove?("w", 0)
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex - 1).get_window().
+              set_bitmap(Bitmap.new("images\\walkRight.bmp"))
+            @mapGrid.get_item(@blockIndex - 1).get_window().
+              set_bitmap(Bitmap.new("images\\wallGreen.bmp"))
+            @mapGrid.get_item(@blockIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@playerIndex - 1)
+            @spaceIndex.push(@blockIndex)
+            @playerIndex = @playerIndex - 1
+            @blockIndex = @blockIndex - 1
+          end
+        end
+        
+      elsif ev.get_key_code() == 83 #S - South
+        
+        spaceAhead = canMove?("s", 0)
+        
+        if playerGrabbing == false
+          playerFacing = "s"
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex + @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\walkDown.bmp"))
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@playerIndex + @numCols)
+            @spaceIndex.push(@playerIndex)
+            @playerIndex = @playerIndex + @numCols
+          else
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\walkDown.bmp"))
+          end
+        elsif playerGrabbing == true && playerFacing == "s"
+          spaceAhead = canMove?("s", 1)
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex + @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\walkDown.bmp"))
+            @mapGrid.get_item(@blockIndex + @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\wallGreen.bmp"))
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@blockIndex + @numCols)
+            @spaceIndex.push(@playerIndex)
+            @playerIndex = @playerIndex + @numCols
+            @blockIndex = @blockIndex + @numCols
+           end
+        elsif playerGrabbing == true && playerFacing == "n"
+          spaceAhead = canMove?("s", 0)
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex + @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\walkUp.bmp"))
+            @mapGrid.get_item(@blockIndex + @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\wallGreen.bmp"))
+            @mapGrid.get_item(@blockIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@playerIndex + @numCols)
+            @spaceIndex.push(@blockIndex)
+            @playerIndex = @playerIndex + @numCols
+            @blockIndex = @blockIndex + @numCols
+          end 
+        end
+        
+      elsif ev.get_key_code() == 68 #D - East
+             
+        if playerGrabbing == false
+          spaceAhead = canMove?("e", 0)
+          playerFacing = "e"
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex + 1).get_window().
+              set_bitmap(Bitmap.new("images\\walkRight.bmp"))
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@playerIndex + 1)
+            @spaceIndex.push(@playerIndex)
+            @playerIndex = @playerIndex + 1
+          else
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\walkRight.bmp"))
+          end
+        elsif playerGrabbing == true && playerFacing == "e"
+          spaceAhead = canMove?("e", 1)
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex + 1).get_window().
+              set_bitmap(Bitmap.new("images\\walkRight.bmp"))
+            @mapGrid.get_item(@blockIndex + 1).get_window().
+              set_bitmap(Bitmap.new("images\\wallGreen.bmp"))
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@blockIndex + 1)
+            @spaceIndex.push(@playerIndex)
+            @playerIndex = @playerIndex + 1
+            @blockIndex = @blockIndex + 1
+           end
+        elsif playerGrabbing == true && playerFacing == "w"
+          spaceAhead = canMove?("e", 0)
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex + 1).get_window().
+              set_bitmap(Bitmap.new("images\\walkLeft.bmp"))
+            @mapGrid.get_item(@blockIndex + 1).get_window().
+              set_bitmap(Bitmap.new("images\\wallGreen.bmp"))
+            @mapGrid.get_item(@blockIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@playerIndex + 1)
+            @spaceIndex.push(@blockIndex)
+            @playerIndex = @playerIndex + 1
+            @blockIndex = @blockIndex + 1
+          end 
+        end
+        
+      elsif ev.get_key_code() == 87 #W - North
+        
+        if playerGrabbing == false
+          spaceAhead = canMove?("n", 0)
+          playerFacing = "n"
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex - @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\walkUp.bmp"))
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@playerIndex - @numCols)
+            @spaceIndex.push(@playerIndex)
+            @playerIndex = @playerIndex - @numCols
+          else
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\walkUp.bmp"))
+          end
+        elsif playerGrabbing == true && playerFacing == "n"
+          spaceAhead = canMove?("n", 1)
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex - @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\walkUp.bmp"))
+            @mapGrid.get_item(@blockIndex - @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\wallGreen.bmp"))
+            @mapGrid.get_item(@playerIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@blockIndex - @numCols)
+            @spaceIndex.push(@playerIndex)
+            @playerIndex = @playerIndex - @numCols
+            @blockIndex = @blockIndex - @numCols
+           end
+        elsif playerGrabbing == true && playerFacing == "s"
+          spaceAhead = canMove?("n", 0)
+          if spaceAhead
+            @mapGrid.get_item(@playerIndex - @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\walkDown.bmp"))
+            @mapGrid.get_item(@blockIndex - @numCols).get_window().
+              set_bitmap(Bitmap.new("images\\wallGreen.bmp"))
+            @mapGrid.get_item(@blockIndex).get_window().
+              set_bitmap(Bitmap.new("images\\basicSquare.bmp"))
+            @spaceIndex.delete(@playerIndex - @numCols)
+            @spaceIndex.push(@blockIndex)
+            @playerIndex = @playerIndex - @numCols
+            @blockIndex = @blockIndex - @numCols
+           end
+         end
+        
+      elsif ev.get_key_code() == 81 #Q - Quit
+        @gameFrame.destroy()
+        
+      elsif ev.get_key_code() == 82 #R - Reset
+        @gameFrame.destroy()
+        loadGame(filename)
+        
+      end }
+      
+  end
+  
+  def canMove?(direction, spaces)
+    
+    
+    if direction == "n"
+      
+      if(@playerIndex - (@numCols + @numCols * spaces) < 0)
+        return false
+      else
+        if @spaceIndex.include?(@playerIndex - (@numCols + @numCols * spaces)) == true
+          return true
+        else
+          return false
+        end
+      end
+      
+    elsif direction == "e"
+      
+      if @spaceIndex.include?(@playerIndex + 1 + spaces) == true
+        return true
+      else
+        return false
+      end
+      
+    elsif direction == "s"
+      
+      if @spaceIndex.include?(@playerIndex + (@numCols + @numCols * spaces)) == true
+        return true
+      else
+        return false
+      end
+      
+    elsif direction == "w"
+
+      if(@playerIndex - (1 + spaces) < 0)
+        return false
+      else
+        if @spaceIndex.include?(@playerIndex - (1 + spaces)) == true
+          return true
+        else
+          return false
+        end
+      end
+      
+    else
+      return false
+      
+    end
   end
   
 end
